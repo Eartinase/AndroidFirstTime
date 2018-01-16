d5b13133b8e2d4e51bbd15c838c76598776c4db4
Orm Wiwitumpon <cutiemoon08@yahoo.com>
2018-01-16 22:05:58 +0700

    Add new activity with change activity button

--- app/src/main/java/com/example/ormwiwitumpon/test/MainActivity.java
+++ app/src/main/java/com/example/ormwiwitumpon/test/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ormwiwitumpon.test;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,20 +19,25 @@ public class MainActivity extends AppCompatActivity {
 
         final Button btnDoSomething =(Button)(findViewById(R.id.btnDoSomething));
         btnDoSomething.setOnClickListener(new View.OnClickListener() {
-       //     TextView text = (TextView) findViewById(R.id.someText);
             TextView num1 = (TextView) findViewById(R.id.editText);
             TextView num2 = (TextView) findViewById(R.id.editText2);
             TextView ans = (TextView) findViewById(R.id.ans);
             int x = 0;
             @Override
             public void onClick(View view) {
-
-                int result = Integer.parseInt(num1.getText().toString())+ Integer.parseInt(num2.getText().toString());
+                int result = Integer.parseInt(num2.getText().toString())- Integer.parseInt(num1.getText().toString());
                 ans.setText("Result is "+result);
              //   text.setText(btnDoSomething.getText());
             //    text.append();
                 x++;
                 Log.i(TAG, "Clicked!");
+            }
+        });
+
+        final Button changeAct = (Button) (findViewById(R.id.changePage));
+        changeAct.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, Activity2.class));
             }
         });
     }
